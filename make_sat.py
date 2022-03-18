9c8c3177f7dbcaf16ed4c033aa941e563e6039a3
hermit <tolokno53@gmail.com>
2022-03-18 19:04:59 +0300

    Ulala

--- make_sat.py
+++ make_sat.py
@@ -57,31 +57,6 @@ def find_sat(n):
         return None
 
 
-class TestSat:
-    def test_one(self):
-        solution = find_sat(1)
-        assert solution == [[1]]
-
-    def test_small(self):
-        for i in range(2, 4):
-            solution = find_sat(i)
-            assert solution is None
-
-    def test_rows(self):
-        for i in range(4, 15):
-            solution = find_sat(i)
-            assert solution is not None
-            assert len(solution) == i
-            for row in solution:
-                assert sum(row) == 1
-
-    def test_columns(self):
-        for i in range(4, 15):
-            solution = find_sat(i)
-            assert solution is not None
-            assert len(solution[0]) == i
-            for row in zip(*solution):
-                assert sum(row) == 1
 
 
 if __name__ == "__main__":
